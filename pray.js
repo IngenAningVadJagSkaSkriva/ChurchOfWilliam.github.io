d0f20f92d917933a1b0e6d167fe4225abdef2484
IngenAningVadJagSkaSkriva <123164118+IngenAningVadJagSkaSkriva@users.noreply.github.com>
2023-10-23 14:15:30 +0200

    Update pray.js

--- pray.js
+++ pray.js
@@ -1,5 +1,19 @@
+var scream = new Audio("scream.mp3");
 var pray = () => {
+    scream.currentTime = 0;
+    scream.play();
     document.open();
-    document.write('<img src="william.webp" style="height:100%; width: 100%;">');
+    document.write('<img src="william.webp" onclick="cansee()" style="height:100%; width: 100%;">');
     alert("STOP FUCKING PRAYING TO ME AND LET ME SLEEP");
+}
+
+var fart = new Audio("fart.mp3");
+var cansee = () => {
+    fart.currentTime = 0;
+    fart.play();
+    alert("DONT TOUCH ME");
+}
+
+var knogleken = () => {
+
 }
